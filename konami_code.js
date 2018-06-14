0b0417371a8e2dd683cacea95cce5ac201a8beb4
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-14 05:27:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -16,7 +16,7 @@ function konami(e) {
   const k = e.key
   if (k === codes[index]) {
     index++;
-    if (index === code.length) {
+    if (index === codes.length) {
       alert('Congratulations, you have found Narnia!');
       index = 0
     }
